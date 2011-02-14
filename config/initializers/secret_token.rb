92f30375933be1bf382e9afabf47ef84f4309b1b
pwolak <wolak.pawel@gmail.com>
2011-02-15 00:15:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandboxApp::Application.config.secret_token = '54de4a67bd1c730edc1f9e944fd0fe5ed4e78f9b0eda103482fde799f6dec7ea1acae23f1e213ef4fcb4547209026c537c67594996d5113c507e63d6a8aa37df'
