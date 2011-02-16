fb796bb9e8593085b9e2dd281f5e715d4f8a7d95
pwolak <wolak.pawel@gmail.com>
2011-02-16 22:14:48 +0100

    Adds price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110215185615) do
+ActiveRecord::Schema.define(:version => 20110216181853) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110215185615) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price",       :precision => 8, :scale => 2, :default => 0.0
   end
 
 end
