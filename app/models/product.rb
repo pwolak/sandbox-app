02d4be94894ebf2b79b5321d53dba6eb5ba2a7e9
pwolak <wolak.pawel@gmail.com>
2011-02-17 17:13:45 +0100

    Adds validations for new product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :image_url
+  validates_uniqueness_of :title
+  validates_numericality_of :price
+  
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.'
+  
+  validate :price_must_be_at_least_a_cent
+  
+protected
+  def price_must_be_at_least_a_cent
+    errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
+  end
 end
