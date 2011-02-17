99e72d0a5de013f26bde9db179579e9c778f232f
pwolak <wolak.pawel@gmail.com>
2011-02-17 22:00:07 +0100

    Creates catalog listing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+
+  def self.find_products_for_sale
+    find(:all, :order => "title")
+  end
+
   validates_presence_of :title, :description, :image_url
   validates_uniqueness_of :title
   validates_numericality_of :price
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sandbox::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
