4d5bbb9f17294613c14c826319fcba8a39d5e22f
pwolak <wolak.pawel@gmail.com>
2011-02-20 19:14:36 +0100

    Creates a smarter cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -7,11 +7,24 @@ class StoreController < ApplicationController
     product = Product.find(params[:id])
     @cart = find_cart
     @cart.add_product(product)
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Invalid product")
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
   end
 
 private
   def find_cart
     session[:cart] ||= Cart.new
+  end
+
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action => 'index'
   end
 
 end
