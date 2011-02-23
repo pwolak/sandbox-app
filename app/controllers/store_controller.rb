dfe09e382bbbc8abbdfbf04c09f13125eb66e7cc
pwolak <wolak.pawel@gmail.com>
2011-02-23 22:00:47 +0100

    Moves the cart to the main page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,7 @@ class StoreController < ApplicationController
 
   def index
     @products =  Product.find_products_for_sale
+    @cart = find_cart
   end
 
   def add_to_cart
@@ -12,6 +13,7 @@ class StoreController < ApplicationController
     if current_user
       @cart.add_product(product)
     end
+    redirect_to_index
   rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
     redirect_to_index("Invalid product")
@@ -27,8 +29,8 @@ private
     session[:cart] ||= Cart.new
   end
 
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg=nil)
+    flash[:notice] = msg if msg
     redirect_to :action => 'index'
   end
 
