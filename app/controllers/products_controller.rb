2478207280e32f6f0c4c9c2003cc6639d09b4334
Mariusz Lusiak <mariusz@applicake.com>
2011-03-04 19:02:58 +0100

    Awesome commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,6 @@ class ProductsController < ApplicationController
   # GET /products/1.xml
   def show
     @product = Product.first
-    @product1 = Product.first
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @product }
